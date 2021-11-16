ae93fe623d535ae637d58aa8fed7b88f5bf4af7e
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-11-17 02:01:34 +0300

    - RoutableLogger Carthage support for all platforms

---
+++ CarthageSupport/RoutableLogger-macOS/RoutableLogger_macOS.h
@@ -0,0 +1,19 @@
+//
+//  RoutableLogger_macOS.h
+//  RoutableLogger-macOS
+//
+//  Created by Anton Plebanovich on 17.11.21.
+//  Copyright © 2021 Anton Plebanovich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RoutableLogger_macOS.
+FOUNDATION_EXPORT double RoutableLogger_macOSVersionNumber;
+
+//! Project version string for RoutableLogger_macOS.
+FOUNDATION_EXPORT const unsigned char RoutableLogger_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoutableLogger_macOS/PublicHeader.h>
+
+
---
+++ CarthageSupport/RoutableLogger-tvOS/RoutableLogger_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  RoutableLogger_tvOS.h
+//  RoutableLogger-tvOS
+//
+//  Created by Anton Plebanovich on 17.11.21.
+//  Copyright © 2021 Anton Plebanovich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RoutableLogger_tvOS.
+FOUNDATION_EXPORT double RoutableLogger_tvOSVersionNumber;
+
+//! Project version string for RoutableLogger_tvOS.
+FOUNDATION_EXPORT const unsigned char RoutableLogger_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoutableLogger_tvOS/PublicHeader.h>
+
+
